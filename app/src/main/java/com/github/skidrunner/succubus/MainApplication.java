78331c8e1c2e1b0f5de0529ff7f3f66624a966dc
skidrunner <skidrunner@gmail.com>
2018-09-24 10:24:03 -0500

    Update for AIDE

--- app/src/main/java/com/github/skidrunner/succubus/MainApplication.java
+++ app/src/main/java/com/github/skidrunner/succubus/MainApplication.java
@@ -19,8 +19,16 @@ import com.jme3.scene.shape.Box;
 import com.jme3.scene.shape.Quad;
 import com.jme3.scene.shape.Sphere;
 import de.lessvoid.nifty.Nifty;
-
 import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import com.jme3.texture.Texture;
+import com.jme3.util.SkyFactory;
+import com.jme3.terrain.Terrain;
+import com.jme3.terrain.geomipmap.TerrainQuad;
+import com.jme3.terrain.geomipmap.TerrainLodControl;
+import com.jme3.terrain.geomipmap.NormalRecalcControl;
+import com.jme3.bullet.control.RigidBodyControl;
+import com.jme3.texture.TextureCubeMap;
 
 public class MainApplication extends SimpleApplication {
 
@@ -36,28 +44,24 @@ public class MainApplication extends SimpleApplication {
 	public void simpleInitApp() {
 		// Create jMonkeyEngine3 Splash Screen
 		createSplashScreen();
-		
+
 		new Thread(new Runnable() {
 				@Override
 				public void run() {
-					destroyFlyByCam();
-					createScene();
-					initNiftyGui();
-					//destroySplash();
+					try {
+						//enqueue(destroyFlyByCam()).get();
+						Nifty nifty = enqueue(initNiftyGui()).get();
+						enqueue(destroySplash()).get();
+						enqueue(createScene()).get();
+						nifty.gotoScreen("main");
+					} catch(InterruptedException | ExecutionException x) {
+						throw new RuntimeException(x);
+					}
 				}
 			}).start();
 
 		cam.setLocation(new Vector3f(1, 1, 2).normalizeLocal().multLocal(8));
 		cam.lookAt(Vector3f.ZERO, Vector3f.UNIT_Y);
-
-		/*
-		 flyCam.setEnabled(false);
-
-
-		 BulletAppState bulletAppState = new BulletAppState();
-		 getStateManager().attach(bulletAppState);
-		 PhysicsSpace physicsSpace = bulletAppState.getPhysicsSpace();
-		 */
 	}
 
 	private void createSplashScreen() {
@@ -85,24 +89,19 @@ public class MainApplication extends SimpleApplication {
 		guiNode.attachChild(node);
 	}
 
-	private void destroyFlyByCam() {
-		try {
-			enqueue(new Callable<Void>() {
-					@Override
-					public Void call() {
-						FlyCamAppState flyCamAppState = getStateManager().getState(FlyCamAppState.class);
-						if(flyCamAppState != null) {
-							getStateManager().detach(flyCamAppState);
-						}
-						return null;
-					}
-				}).get();
-		} catch(Exception e) {
-			//throw new RuntimeException(e);
-		}
+	private Callable<Void> destroyFlyByCam() {
+		return new Callable<Void>() {
+			public Void call() {
+				FlyCamAppState flyCamAppState = getStateManager().getState(FlyCamAppState.class);
+				if(flyCamAppState != null) {
+					getStateManager().detach(flyCamAppState);
+				}
+				return null;
+			}
+		};
 	}
 
-	private void initNiftyGui() {
+	private Callable<Nifty> initNiftyGui() {
 		final NiftyJmeDisplay niftyProcessor = new NiftyJmeDisplay(
 			getAssetManager(), getInputManager(),
 			getAudioRenderer(), getGuiViewPort());
@@ -121,86 +120,64 @@ public class MainApplication extends SimpleApplication {
 
 		nifty.enableAutoScaling(((int)(width)), ((int)(height)));
 		nifty.fromXml("Interface/Screens.xml", "start");
-		try {
-			enqueue(new Callable<Void>() {
-					@Override
-					public Void call() {
-						getGuiViewPort().addProcessor(niftyProcessor);
-						return null;
-					}
-				}).get();
-		} catch(Exception e) {
-			throw new RuntimeException(e);
-		}
-		destroySplash();
-		try {
-			enqueue(new Callable<Void>() {
-				@Override
-				public Void call() {
-					nifty.gotoScreen("main");
-					return null;
+		return new Callable<Nifty>() {
+			@Override
+			public Nifty call() {
+				getGuiViewPort().addProcessor(niftyProcessor);
+				return nifty;
+			}
+		};
+	}
+
+	private Callable<Void> destroySplash() {
+		return new Callable<Void>() {
+			@Override
+			public Void call() {
+				Spatial spatial = guiNode.getChild("Splash Screen");
+				if(spatial != null) {
+					spatial.removeFromParent();
 				}
-				}).get();
-		} catch(Exception e) {
-			throw new RuntimeException(e);
-		}
+				return null;
+			}
+		};
 	}
 
-	private void destroySplash() {
-		try {
-			enqueue(new Callable<Void>() {
-					@Override
-					public Void call() {
-						Spatial spatial = guiNode.getChild("Splash Screen");
-						if(spatial != null) {
-							spatial.removeFromParent();
-						}
-						return null;
-					}
-				}).get();
-		} catch(Exception e) {
-			throw new RuntimeException(e);
-		}
-	}
-
-	private void createScene() {
+	private Callable<Void> createScene() {
 		final Node scene = new Node("Scene");
 		
+		Material defaultMaterial = assetManager.loadMaterial("Materials/Default.j3m");
+		
 		DirectionalLight light = new DirectionalLight();
+		light.setDirection(new Vector3f(0.5f, -1.0f, 0.0f).normalizeLocal());
 		light.setColor(new ColorRGBA(1.0f, 0.96f, 0.93f, 1.0f));
 		scene.addLight(light);
-
-		Material material = new Material(assetManager, "Common/MatDefs/Light/Lighting.j3md");
-		material.setBoolean("UseMaterialColors", true);
-		material.setColor("Diffuse", new ColorRGBA(0, 0.5f, 0, 1));
 		
-		Geometry geometry = new Geometry("Geometry0", new Box(128, 0.5f, 128));
-		geometry.setMaterial(material); 
-		geometry.setLocalTranslation(0, -0.5f, 0);
-		scene.attachChild(geometry);
+		Texture skyWest = assetManager.loadTexture("Textures/Skybox/West.png");
+		Texture skyEast = assetManager.loadTexture("Textures/Skybox/East.png");
+		Texture skyNorth = assetManager.loadTexture("Textures/Skybox/North.png");
+		Texture skySouth = assetManager.loadTexture("Textures/Skybox/South.png");
+		Texture skyTop = assetManager.loadTexture("Textures/Skybox/Top.png");
+		Texture skyBottom = assetManager.loadTexture("Textures/Skybox/Bottom.png");
+		Geometry skybox = ((Geometry)(SkyFactory.createSky(assetManager, skyWest, skyEast, skyNorth, skySouth, skyTop, skyBottom)));
+		TextureCubeMap skyTexture = ((TextureCubeMap)(skybox.getMaterial().getTextureParam("Texture").getTextureValue()));
+		defaultMaterial.setTexture("EnvMap", skyTexture);
+		defaultMaterial.setVector3("FresnelParams", new Vector3f(0.5f, 2.0f, 2.0f));
+		skybox.setName("Skybox");
+		scene.attachChild(skybox);
 		
-		scene.attachChild(createSky());
-		try {
-			enqueue(new Callable<Void>() {
-					@Override
-					public Void call() {
-						rootNode.attachChild(scene);
-						return null;
-					}
-				}).get();
-		} catch(Exception e) {
-			//throw new RuntimeException(e);
-		}
+		TerrainQuad terrain = new TerrainQuad("Terrain", 65, 257, new float[256 * 256]);
+		terrain.addControl(new TerrainLodControl(terrain, cam));
+		terrain.addControl(new NormalRecalcControl(terrain));
+		terrain.addControl(new RigidBodyControl(0));
+		terrain.setMaterial(defaultMaterial);
+		scene.attachChild(terrain);
 		
-	}
-	
-	private Spatial createSky() {
-		Mesh sphereMesh = new Sphere(18, 36, 16, true, true);
-		Geometry sky = new Geometry("Sky", sphereMesh);
-        sky.setQueueBucket(RenderQueue.Bucket.Sky);
-        sky.setCullHint(Spatial.CullHint.Never);
-        sky.setModelBound(new BoundingSphere(Float.POSITIVE_INFINITY, Vector3f.ZERO));
-		sky.setMaterial(assetManager.loadMaterial("Materials/DefaultSky.j3m"));
-        return sky;
+		return new Callable<Void>() {
+			@Override
+			public Void call() {
+				rootNode.attachChild(scene);
+				return null;
+			}
+		};
 	}
 }
